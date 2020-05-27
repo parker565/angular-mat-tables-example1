58bb08d0c115e1302d704fb612de06e8b56f62a0
Parker Chowdary <parkerchowdary@gmail.com>
2020-05-26 23:02:30 -0400

    initail commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TableComponent } from './components/table/table.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TableComponent},
+  {path: 'table', component: TableComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/material.module.ts
@@ -0,0 +1,16 @@
+// material.module.ts
+
+import { NgModule } from '@angular/core';
+
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  imports: [
+    MatTableModule
+  ],
+  exports: [
+    MatTableModule
+  ]
+})
+
+export class MaterialModule {}
